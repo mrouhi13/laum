0b50eac07d1fb869c1fef0118cf38a5e2bd5e8f8
mrouhi13 <mrouhi13@icloud.com>
2018-06-23 16:43:45 +0430

    Add Result and Content page design and some improvements

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,10 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls.conf import path
 
 from . import views
 
 app_name = 'webapp'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('/', views.DataListView.as_view(), name='list'),
-    path('<int:pk>/', views.DataDetailView.as_view(), name='detail'),
-]
+                  path('', views.index, name='index'),
+                  path('search/', views.DataListView.as_view(), name='list'),
+                  path('<int:pk>/', views.DataDetailView.as_view(), name='detail'),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
