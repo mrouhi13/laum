a72a4d975cf6cab4763e777217a0aa6c8be5fd90
mrouhi13 <mrouhi13@icloud.com>
2018-07-01 12:37:42 +0430

    Create tag model

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Data
+from .models import Data, Tag
 
 admin.site.register(Data)
+admin.site.register(Tag)
--- webapp/models.py
+++ webapp/models.py
@@ -1,7 +1,23 @@
 import random
+import string
 
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+
+
+def generate_new_pid(n=12):
+    new_pid_passed = False
+    new_pid = ''
+    prefix_string = '0'
+
+    while not new_pid_passed:
+        postfix_string = ''.join(random.choices(string.ascii_letters + string.digits, k=n))
+        new_pid = '%s%s' % (prefix_string, postfix_string)
+
+        if not Data.objects.is_pid_exists(new_pid):
+            new_pid_passed = True
+
+    return new_pid
 
 
 class DataManager(models.Manager):
@@ -15,18 +31,22 @@ class DataManager(models.Manager):
 
         return random_data
 
+    def is_pid_exists(self, pid):
+        return self.filter(pid=pid).exists()
+
 
 class Data(models.Model):
-    pid = models.CharField(_('شناسه‌ی عمومی'), max_length=13, unique=True)
+    tag = models.ManyToManyField('Tag', related_name='tags', related_query_name="tag")
+    pid = models.CharField(_('شناسه‌ی عمومی'), max_length=13, unique=True, default=generate_new_pid)
     title = models.CharField(_('عنوان'), max_length=128)
-    subtitle = models.CharField(_('زیرعنوان'), max_length=128, blank=True)
+    subtitle = models.CharField(_('زیرعنوان'), max_length=128, blank=True, default='')
     content = models.TextField(_('محتوا'), max_length=1024)
-    ann_date = models.CharField(_('تاریخ'), max_length=128, blank=True)
-    image = models.ImageField(_('تضویر'), upload_to='images', blank=True)
-    image_caption = models.CharField(_('توضیح تصویر'), max_length=128, blank=True)
-    reference = models.CharField(_('منبع'), max_length=128, blank=True)
-    author = models.EmailField(_('نویسنده'), max_length=254, blank=True)
-    # status = models.BooleanField(_('status'), default=False)
+    ann_date = models.CharField(_('تاریخ'), max_length=128, blank=True, default='')
+    image = models.ImageField(_('تضویر'), upload_to='images', blank=True, default='')
+    image_caption = models.CharField(_('توضیح تصویر'), max_length=128, blank=True, default='')
+    reference = models.CharField(_('منبع'), max_length=128, blank=True, default='')
+    author = models.EmailField(_('نویسنده'), max_length=254, blank=True, default='')
+    status = models.BooleanField(_('وضعیت'), default=False)
     updated_on = models.DateTimeField(_('آخرین به‌روزرسانی'), auto_now=True)
     created_on = models.DateTimeField(_('تاریخ ایجاد'), auto_now_add=True)
 
@@ -39,3 +59,19 @@ class Data(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(_('نام'), max_length=128)
+    status = models.BooleanField(_('وضعیت'), default=False)
+    updated_on = models.DateTimeField(_('آخرین به‌روزرسانی'), auto_now=True)
+    created_on = models.DateTimeField(_('تاریخ ایجاد'), auto_now_add=True)
+
+    objects = DataManager()
+
+    class Meta:
+        verbose_name = "برچسب"
+        verbose_name_plural = "برچسب‌ها"
+
+    def __str__(self):
+        return self.name
