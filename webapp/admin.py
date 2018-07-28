0b6220405936008c54e5a16a4c1af7945f17b638
Majid Rouhi <mrouhi13@icloud.com>
2018-07-28 20:30:03 +0430

    Code improvements

--- webapp/admin.py
+++ webapp/admin.py
@@ -3,7 +3,14 @@ from django.contrib.auth.models import Group
 
 from .models import Data, Tag, Report
 
+admin.site.site_header = 'مدیریت لام'
+admin.site.site_title = 'مدیریت لام'
+admin.site.index_title = 'مدیریت لام'
 
+admin.site.unregister(Group)
+
+
+@admin.register(Data)
 class DataAdmin(admin.ModelAdmin):
     readonly_fields = ('pid', 'jalali_updated_on', 'jalali_created_on')
     fieldsets = [
@@ -16,6 +23,7 @@ class DataAdmin(admin.ModelAdmin):
     search_fields = ['title', 'content', 'event', 'image_caption', 'tag']
 
 
+@admin.register(Report)
 class ReportAdmin(admin.ModelAdmin):
     readonly_fields = ('data', 'jalali_updated_on', 'jalali_created_on')
     fieldsets = [
@@ -27,6 +35,7 @@ class ReportAdmin(admin.ModelAdmin):
     search_fields = ['data', 'body', 'reporter']
 
 
+@admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     readonly_fields = ('keyword', 'jalali_updated_on', 'jalali_created_on')
     fieldsets = [
@@ -36,13 +45,3 @@ class TagAdmin(admin.ModelAdmin):
     list_display = ('name', 'keyword', 'is_active', 'jalali_updated_on', 'jalali_created_on')
     list_filter = ['is_active', 'updated_on', 'created_on']
     search_fields = ['name', 'keyword']
-
-
-admin.site.register(Data, DataAdmin)
-admin.site.register(Report, ReportAdmin)
-admin.site.register(Tag, TagAdmin)
-admin.site.unregister(Group)
-
-admin.site.site_header = 'مدیریت لـامـ'
-admin.site.site_title = 'مدیریت لـامـ'
-admin.site.index_title = 'مدیریت لـامـ'
