799cafb0be59b42eff3179946f2ef789bdaffe93
Majid Rouhi <mrouhi13@icloud.com>
2018-10-27 22:10:39 +0330

    Make site title dynamic

---
+++ web/context_processors.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+
+
+def site_info(request):
+    info = {
+        'site_title': settings.SITE_TITLE,
+        'site_slogan': settings.SITE_SLOGAN,
+        'site_url': settings.SITE_URL,
+        'contact_email': settings.CONTACT_EMAIL
+    }
+
+    return info
