7bcf10c9ead79e2ab4e47e01a9c9a1ed8edf7e33
Majid Rouhi <mrouhi13@icloud.com>
2018-11-02 19:44:24 +0330

    Add robots.txt

--- web/urls.py
+++ web/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
 from django.urls.conf import path
+from django.views.generic import TemplateView
 
 from . import views
 from .sitemaps import PageSitemap, StaticViewSitemap
@@ -19,6 +20,7 @@ urlpatterns = [
     path('page/create/', views.PageCreateView.as_view(), name='page-create'),
     path('report/create/', views.ReportCreateView.as_view(), name='report-create'),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
+    path('robots.txt', TemplateView.as_view(template_name='web/robots.txt', content_type='text/plain')),
 ]
 
 # This is only needed when using runserver.
