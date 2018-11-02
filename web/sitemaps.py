afcc84bd3419b86e9b4dfcdd6bb0f59aeaca5ee5
Majid Rouhi <mrouhi13@icloud.com>
2018-11-02 19:42:24 +0330

    Add Django sitemap

---
+++ web/sitemaps.py
@@ -0,0 +1,31 @@
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+
+from .models import Page
+
+
+class PageSitemap(Sitemap):
+    changefreq = 'daily'
+    priority = 0.9
+    protocol = 'https'
+
+    def items(self):
+        return Page.objects.filter(is_active=True)
+
+    def lastmod(self, obj):
+        return obj.updated_on
+
+    def location(self, obj):
+        return '/{}/'.format(obj.pid)
+
+
+class StaticViewSitemap(Sitemap):
+    priority = 0.5
+    changefreq = 'daily'
+    protocol = 'https'
+
+    def items(self):
+        return ['web:index', 'web:page-list']
+
+    def location(self, item):
+        return reverse(item)
--- web/urls.py
+++ web/urls.py
@@ -1,8 +1,15 @@
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
 from django.urls.conf import path
 
 from . import views
+from .sitemaps import PageSitemap, StaticViewSitemap
+
+sitemaps = {
+    'pages': PageSitemap,
+    'statics': StaticViewSitemap
+}
 
 app_name = 'web'
 urlpatterns = [
@@ -11,6 +18,7 @@ urlpatterns = [
     path('<str:slug>/', views.PageDetailView.as_view(), name='page-detail'),
     path('page/create/', views.PageCreateView.as_view(), name='page-create'),
     path('report/create/', views.ReportCreateView.as_view(), name='report-create'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
 ]
 
 # This is only needed when using runserver.
