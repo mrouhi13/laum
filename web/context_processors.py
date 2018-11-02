89b7d9e7673c3557d1435cf3bfe445e1fbc57e7f
Majid Rouhi <mrouhi13@icloud.com>
2018-11-02 22:14:01 +0330

    HTML tags optimization

--- web/context_processors.py
+++ web/context_processors.py
@@ -6,7 +6,8 @@ def site_info(request):
         'site_title': settings.SITE_TITLE,
         'site_slogan': settings.SITE_SLOGAN,
         'site_url': settings.SITE_URL,
-        'contact_email': settings.CONTACT_EMAIL
+        'contact_email': settings.CONTACT_EMAIL,
+        'language_code': settings.LANGUAGE_CODE
     }
 
     return info
