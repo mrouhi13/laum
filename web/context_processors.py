d7bba15f66a5f324b3a6e33ea46ab07a6ce404e2
Majid Rouhi <mrouhi13@gmail.com>
2018-11-04 00:34:59 +0330

    HTML tags and SEO optimization

--- web/context_processors.py
+++ web/context_processors.py
@@ -4,10 +4,12 @@ from django.conf import settings
 def site_info(request):
     info = {
         'site_title': settings.SITE_TITLE,
-        'site_slogan': settings.SITE_SLOGAN,
+        'site_slogan_1': settings.SITE_SLOGAN_1,
+        'site_slogan_2': settings.SITE_SLOGAN_2,
         'site_url': settings.SITE_URL,
         'contact_email': settings.CONTACT_EMAIL,
-        'language_code': settings.LANGUAGE_CODE
+        'language_code': settings.LANGUAGE_CODE,
+        'google_analytics_id': settings.GOOGLE_ANALYTICS_ID
     }
 
     return info
