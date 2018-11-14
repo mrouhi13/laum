89febcdc9c5291607ec92b5790a6974fab1e4693
Majid Rouhi <mrouhi13@gmail.com>
2018-11-15 00:53:56 +0330

    Make meta tags and canonical link dynamic

--- web/context_processors.py
+++ web/context_processors.py
@@ -7,6 +7,8 @@ def site_info(request):
         'site_slogan_1': settings.SITE_SLOGAN_1,
         'site_slogan_2': settings.SITE_SLOGAN_2,
         'site_url': settings.SITE_URL,
+        'default_description': settings.DEFAULT_DESCRIPTION,
+        'default_keywords': settings.DEFAULT_KEYWORDS,
         'contact_email': settings.CONTACT_EMAIL,
         'language_code': settings.LANGUAGE_CODE,
         'google_analytics_id': settings.GOOGLE_ANALYTICS_ID
